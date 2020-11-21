9769e99b837435994c1c82075b18124c82f2c7ba
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2020-11-21 17:52:55 +0100

    Init
    dodaj gitignore ktoś

---
+++ server_app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WASIK");
+            Console.Read();
+        }
+    }
+}
