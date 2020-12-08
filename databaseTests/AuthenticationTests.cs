adc451b9b1e022a4eabb538579056dcf0c90ca05
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2020-12-08 05:27:37 +0100

    Tests

---
+++ databaseTests/AuthenticationTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database.Tests
+{
+    [TestClass()]
+    public class AuthenticationTests
+    {
+        [TestMethod()]
+        public void AuthorizeUserTest()
+        {
+            Authentication auth = new Authentication();
+            //user and password exist so no exception should be thrown
+            auth.AuthorizeUser("Kret", "e2f0c2aafca651a80fe70ca7159ad93a2915e9a99cf34b1eebd0412aec2e3dac");
+            //user and password don't exist- throws exception
+            Assert.ThrowsException<AuthenticationException>(() => auth.AuthorizeUser("test", "niema"));
+        }
+
+        [TestMethod()]
+        public void ConnectToDatabaseTest()
+        {
+            DatabaseConector d = new DatabaseConector();
+            d.ConnectToDatabase();
+            d.DisconectFromDatabase();
+        }
+    }
+}
---
+++ packagesTests/PackageTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using packages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace packages.Tests
+{
+    [TestClass()]
+    public class PackageTests
+    {
+        [TestMethod()]
+        public void getArgumentsTest()
+        {
+            String data = "<PACKAGE>" +
+                "         <TYPE>LOGIN_CONFIRM</TYPE>" +
+                "         <arg1>example</arg1>" +
+                "         </PACKAGE>";
+            Package p = new Communication_Package(Encoding.ASCII.GetBytes(data));
+
+            List<String> arguments = p.getArguments();
+            List<String> expectedResult = new List<String>() { "LOGIN_CONFIRM", "example" };
+            CollectionAssert.AreEqual(arguments, expectedResult);
+        }
+
+    }
+}
